2b74fc1528816820fba3ee07b510968306ecad3f
Otlet <otlet@protonmail.com>
2016-05-31 11:30:25 +0200

    SEO

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\SeoBundle\SonataSeoBundle(),
             new AppBundle\AppBundle(),
         ];
 
