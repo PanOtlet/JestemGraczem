774e13ecea4dd812362f28d6197ad3b73151b198
Otlet <otlet@protonmail.com>
2016-05-31 13:15:18 +0200

    Profile editing

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,9 +19,113 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     minMessage="The name is too short.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $steam = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     minMessage="The name is too short.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $battlenet = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     minMessage="The name is too short.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $lol = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     minMessage="The name is too short.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $twitch = null;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     minMessage="The name is too short.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $localization = null;
+
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+    }
+
+    public function getBattlenet()
+    {
+        return $this->battlenet;
+    }
+
+    public function getSteam()
+    {
+        return $this->steam;
+    }
+
+    public function getLol()
+    {
+        return $this->lol;
+    }
+
+    public function getTwitch()
+    {
+        return $this->twitch;
+    }
+
+    public function getLocalization()
+    {
+        return $this->localization;
+    }
+
+    public function setSteam($steam)
+    {
+        $this->steam = $steam;
+    }
+
+    public function setBattlenet($battlenet)
+    {
+        $this->battlenet = $battlenet;
+    }
+
+    public function setLol($lol)
+    {
+        $this->lol = $lol;
+    }
+
+    public function setTwitch($twitch)
+    {
+        $this->twitch = $twitch;
+    }
+
+    public function setLocalization($localization)
+    {
+        $this->localization = $localization;
     }
 }
