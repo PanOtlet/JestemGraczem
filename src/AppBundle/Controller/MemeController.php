a9c11e4e85db905717b2fb51a90ffcfd248fb256
Otlet <otlet@protonmail.com>
2016-06-01 15:38:34 +0200

    MEME

---
+++ src/AppBundle/Controller/MemeController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use AppBundle\Entity\News;
+
+class MemeController extends Controller
+{
+    /**
+     * @Route("/meme/{page}", name="meme")
+     */
+    public function indexAction($page=1)
+    {
+        $em = $this->getDoctrine()->getRepository('AppBundle:Meme');
+        $query = $em->createQueryBuilder('p')
+            ->where('p.status > 0')
+            ->where('p.id > :min')
+            ->setParameter('min', $page*10)
+            ->orderBy('p.id', 'DESC')
+            ->getQuery()->setMaxResults(10);
+
+        $meme = $query->getResult();
+        
+        return $this->render('meme/index.html.twig',['meme' => $meme]);
+    }
+
+    /**
+     * @Route("/meme/img/{id}", name="meme.id")
+     */
+    public function memAction()
+    {
+        if (isset($_GET['url']) && !preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $_GET['url'])) {
+            return $this->redirectToRoute('homepage');
+        }
+        return $this->redirect($_GET['url']);
+    }
+
+    /**
+     * @Route("/meme/add", name="meme.add")
+     */
+    public function addAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('url', UrlType::class, array('label' => 'Adres RSS', 'required' => true))
+            ->add('name', TextType::class, array('label' => 'Nazwa', 'required' => true))
+            ->add('css', TextType::class, array('label' => 'Kolor', 'required' => true, 'attr' => ['class' => 'jscolor']))
+            ->add('save', SubmitType::class, array('label' => 'Dodaj kanał'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = new News();
+            $data->setUser($this->getUser()->getId());
+            $data->setName($form->get('name')->getViewData());
+            $data->setUrl($form->get('url')->getViewData());
+            $data->setCss($form->get('css')->getViewData());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($data);
+            $em->flush();
+
+            $this->addFlash(
+                'danger',
+                'Dodano prywatny kanał informacji!'
+            );
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/newsadd.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/AppBundle/Entity/Meme.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Meme
+ *
+ * @ORM\Table(name="meme")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MemeRepository")
+ */
+class Meme
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user", type="integer")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file", type="string", length=255, unique=true)
+     */
+    private $file;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="category", type="integer")
+     */
+    private $category;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="points", type="integer")
+     */
+    private $points;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param integer $user
+     *
+     * @return Meme
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return int
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Meme
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     *
+     * @return Meme
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Meme
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set category
+     *
+     * @param integer $category
+     *
+     * @return Meme
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return int
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Meme
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set points
+     *
+     * @param integer $points
+     *
+     * @return Meme
+     */
+    public function setPoints($points)
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    /**
+     * Get points
+     *
+     * @return int
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+}
+
