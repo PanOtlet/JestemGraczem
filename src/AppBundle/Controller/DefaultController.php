cbd2262dfd0e80b7cbd53fdddd88501e3d56472d
Otlet <otlet@protonmail.com>
2016-06-01 10:55:56 +0200

    Add News

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use AppBundle\Entity\News;
 
 class DefaultController extends Controller
 {
@@ -27,6 +32,44 @@ class DefaultController extends Controller
             return $this->redirectToRoute('homepage');
         }
         return $this->redirect($_GET['url']);
+    }
+
+    /**
+     * @Route("/news/add", name="news.add")
+     */
+    public function newsAddAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('url', UrlType::class, array('label' => 'Adres RSS'))
+            ->add('name', TextType::class, array('label' => 'Nazwa'))
+            ->add('css', TextType::class, array('label' => 'Kolor', 'attr' => ['class'=>'jscolor']))
+            ->add('save', SubmitType::class, array('label' => 'Dodaj kanał'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = new News();
+            $data->setUser($this->getUser()->getId());
+            $data->setName($form->get('name')->getViewData());
+            $data->setUrl($form->get('url')->getViewData());
+            $data->setCss($form->get('css')->getViewData());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($data);
+            $em->flush();
+
+            $this->addFlash(
+                'danger',
+                'Dodano prywatny kanał informacji!'
+            );
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/newsadd.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
---
+++ src/AppBundle/Entity/News.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="news")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user", type="integer")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=25)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="css", type="string", length=255)
+     */
+    private $css;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Get user
+     *
+     * @return int
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     *
+     * @return News
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return News
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return News
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set css
+     *
+     * @param string $css
+     *
+     * @return News
+     */
+    public function setCss($css)
+    {
+        $this->css = $css;
+
+        return $this;
+    }
+
+    /**
+     * Get css
+     *
+     * @return string
+     */
+    public function getCss()
+    {
+        return $this->css;
+    }
+}
+
