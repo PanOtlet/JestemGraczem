db7050d087af6ff8f97fc9e202894957437a45f6
Otlet <otlet@protonmail.com>
2016-06-09 12:48:04 +0200

    Much edits

---
+++ src/TurniejBundle/Controller/DivisionController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace TurniejBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use TurniejBundle\Entity\Division;
+use TurniejBundle\Entity\Team;
+
+class DivisionController extends Controller
+{
+    /**
+     * @Route("/add", name="division.add")
+     */
+    public function addAction()
+    {
+
+        return $this->render('team/add.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/join", name="team.join")
+     */
+    public function joinAction(Request $request)
+    {
+
+        $team = new Division();
+
+        $form = $this->createFormBuilder($team)
+            ->add('name', NULL, [
+                'label' => 'Nazwa',
+                'attr' => [
+                    'placeholder' => 'Podaj pełną nazwę dywizji!'
+                ]
+            ])
+            ->add('pass', NULL, [
+                'label' => 'Hasło dostępowe'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Dodaj',
+                'attr' => [
+                    'class' => 'btn-raised btn-danger'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $division = $this->getDoctrine()->getRepository('TurniejBundle:Division')->findOneBy([
+                'name' => $form->get('name')->getViewData(),
+                'pass' => $form->get('pass')->getViewData()
+            ]);
+
+            if ($division == NULL) {
+                $this->addFlash(
+                    'danger',
+                    'Źle wprowadzona nazwa lub hasło! Upewnij się, że podajesz poprawne dane!'
+                );
+                return $this->redirectToRoute('team');
+            }
+
+            $members = $division->getMembers();
+
+            foreach ($members as $member) {
+                if ($member['user'] == $this->getUser()->getUsername()) {
+                    $this->addFlash(
+                        'danger',
+                        'Już jesteś w drużynie!'
+                    );
+                    return $this->redirectToRoute('division', ['id' => $division->getName()]);
+                }
+            }
+
+            $members[] = [
+                'user' => $this->getUser()->getUsername(),
+                'role' => 'Nowy'
+            ];
+
+            $division->setMembers($members);
+
+            $em->persist($division);
+            $em->flush();
+
+            $this->addFlash(
+                'danger',
+                'Gratulacje! Dostałeś się do drużyny!'
+            );
+            return $this->redirectToRoute('division', ['id' => $division->getName()]);
+        }
+
+        return $this->render('team/join.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="division")
+     */
+    public function divisionAction($id = NULL)
+    {
+        $team = $this->getDoctrine()->getRepository('TurniejBundle:Division')->findOneBy(['name' => $id]);
+
+        if ($id == NULL || $team == NULL) {
+            return $this->redirectToRoute('tournament');
+        }
+
+        return $this->render('team/division.html.twig', [
+            'team' => $team
+        ]);
+    }
+}
--- src/TurniejBundle/Controller/TeamController.php
+++ src/TurniejBundle/Controller/TeamController.php
@@ -12,26 +12,18 @@ use TurniejBundle\Entity\Team;
 class TeamController extends Controller
 {
     /**
-     * @Route("/", name="teams")
+     * @Route("/{tag}", name="team")
      */
-    public function indexAction()
-    {
-        $teams = $this->getDoctrine()->getRepository('TurniejBundle:Team')->findAll();
-
-        return $this->render('team/teams.html.twig', [
-            'teams' => $teams
-        ]);
-    }
-
-    /**
-     * @Route("/druzyna/{tag}", name="team")
-     */
-    public function teamAction($tag = NULL)
+    public function indexAction($tag = NULL)
     {
         $team = $this->getDoctrine()->getRepository('TurniejBundle:Team')->findOneBy(['tag' => $tag]);
 
         if ($tag == NULL || $team == NULL) {
-            return $this->redirectToRoute('tournament');
+            $teams = $this->getDoctrine()->getRepository('TurniejBundle:Team')->findAll();
+
+            return $this->render('team/teams.html.twig', [
+                'teams' => $teams
+            ]);
         }
 
         $owner = $this->getDoctrine()->getRepository('AppBundle:User')->findOneBy(['id' => $team->getOwner()]);
@@ -45,15 +37,15 @@ class TeamController extends Controller
     }
 
     /**
-     * @Route("/druzyna/{tag}/remove", name="team.remove")
+     * @Route("/{tag}/edit", name="team.edit")
      */
-    public function removeAction($tag = NULL)
+    public function editAction($tag = NULL)
     {
         $em = $this->getDoctrine()->getManager();
         $team = $em->getRepository('TurniejBundle:Team')->findOneBy(['tag' => $tag]);
 
         if ($tag == NULL || $team == NULL) {
-            return $this->redirectToRoute('teams');
+            return $this->redirectToRoute('team');
         }
 
         if ($this->getUser()->getId() != $team->getOwner()) {
@@ -61,7 +53,7 @@ class TeamController extends Controller
                 'danger',
                 'Nie jesteś właścicielem drużyny!'
             );
-            return $this->redirectToRoute('teams');
+            return $this->redirectToRoute('team');
         }
 
         $em->remove($team);
@@ -71,23 +63,40 @@ class TeamController extends Controller
             'danger',
             'Drużyna została usunięta!'
         );
-        return $this->redirectToRoute('teams');
+
+        return $this->render('team/edit.html.twig', [
+
+        ]);
     }
 
     /**
-     * @Route("/dywizja/{id}", name="division")
+     * @Route("/{tag}/remove", name="team.remove")
      */
-    public function divisionAction($id = NULL)
+    public function removeAction($tag = NULL)
     {
-        $team = $this->getDoctrine()->getRepository('TurniejBundle:Division')->findOneBy(['name' => $id]);
+        $em = $this->getDoctrine()->getManager();
+        $team = $em->getRepository('TurniejBundle:Team')->findOneBy(['tag' => $tag]);
 
-        if ($id == NULL || $team == NULL) {
-            return $this->redirectToRoute('tournament');
+        if ($tag == NULL || $team == NULL) {
+            return $this->redirectToRoute('team');
         }
 
-        return $this->render('team/division.html.twig', [
-            'team' => $team
-        ]);
+        if ($this->getUser()->getId() != $team->getOwner()) {
+            $this->addFlash(
+                'danger',
+                'Nie jesteś właścicielem drużyny!'
+            );
+            return $this->redirectToRoute('team');
+        }
+
+        $em->remove($team);
+        $em->flush();
+
+        $this->addFlash(
+            'danger',
+            'Drużyna została usunięta!'
+        );
+        return $this->redirectToRoute('team');
     }
 
     /**
@@ -98,95 +107,6 @@ class TeamController extends Controller
 
         return $this->render('team/add.html.twig', [
 
-        ]);
-    }
-
-    /**
-     * @Route("/edit/{tag}", name="team.edit")
-     */
-    public function editAction()
-    {
-        return $this->render('team/edit.html.twig', [
-
-        ]);
-    }
-
-    /**
-     * @Route("/join", name="team.join")
-     */
-    public function joinAction(Request $request)
-    {
-
-        $team = new Division();
-
-        $form = $this->createFormBuilder($team)
-            ->add('name', NULL, [
-                'label' => 'Nazwa',
-                'attr' => [
-                    'placeholder' => 'Podaj pełną nazwę dywizji!'
-                ]
-            ])
-            ->add('pass', NULL, [
-                'label' => 'Hasło dostępowe'
-            ])
-            ->add('save', SubmitType::class, [
-                'label' => 'Dodaj',
-                'attr' => [
-                    'class' => 'btn-raised btn-danger'
-                ]
-            ])
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $em = $this->getDoctrine()->getManager();
-
-            $division = $this->getDoctrine()->getRepository('TurniejBundle:Division')->findOneBy([
-                'name' => $form->get('name')->getViewData(),
-                'pass' => $form->get('pass')->getViewData()
-            ]);
-
-            if ($division == NULL) {
-                $this->addFlash(
-                    'danger',
-                    'Źle wprowadzona nazwa lub hasło! Upewnij się, że podajesz poprawne dane!'
-                );
-                return $this->redirectToRoute('teams');
-            }
-
-            $members = $division->getMembers();
-
-            foreach ($members as $member) {
-                if ($member['user'] == $this->getUser()->getUsername()) {
-                    $this->addFlash(
-                        'danger',
-                        'Już jesteś w drużynie!'
-                    );
-                    return $this->redirectToRoute('division', ['id' => $division->getName()]);
-                }
-            }
-
-            $members[] = [
-                'user' => $this->getUser()->getUsername(),
-                'role' => 'Nowy'
-            ];
-
-            $division->setMembers($members);
-
-            $em->persist($division);
-            $em->flush();
-
-            $this->addFlash(
-                'danger',
-                'Gratulacje! Dostałeś się do drużyny!'
-            );
-            return $this->redirectToRoute('division', ['id' => $division->getName()]);
-        }
-
-        return $this->render('team/join.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 
