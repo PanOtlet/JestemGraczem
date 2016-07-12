ebbf4e2d0f3e590ce617090a323db874563d0a5b
Otlet <otlet@protonmail.com>
2016-07-12 13:48:29 +0200

    CMS

---
+++ src/AppBundle/Controller/CMSController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class CMSController extends Controller
+{
+    /**
+     * @Route("/{title}", name="cms")
+     */
+    public function indexAction($title = NULL)
+    {
+        $cms = $this->getDoctrine()->getRepository('AppBundle:CMS')->findOneBy(['url' => $title]);
+
+        if ($title == NULL || $cms == NULL) {
+            throw $this->createNotFoundException('Strony nie znaleziono!');
+        }
+
+        $seo = $this->container->get('sonata.seo.page');
+        $seo->setTitle($cms->getTitle() . ' :: JestemGraczem.pl')
+            ->addMeta('name', 'description', substr($cms->getText(), 0, 150))
+            ->addMeta('property', 'og:title', $cms->getTitle() . ' :: JestemGraczem.pl')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('cms', ['title' => $cms->getUrl()], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        return $this->render('CMS/index.html.twig', [
+            'cms' => $cms
+        ]);
+    }
+
+}
---
+++ src/AppBundle/Entity/CMS.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CMS
+ *
+ * @ORM\Table(name="c_m_s")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CMSRepository")
+ */
+class CMS
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return CMS
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return CMS
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return CMS
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
+
