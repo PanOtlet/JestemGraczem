79c344dec2067b995958b326d3c1539aa7fdbbd2
Otlet <otlet@protonmail.com>
2016-07-29 11:32:43 +0200

    HotS

--- src/AppBundle/Controller/GameCenterController.php
+++ src/AppBundle/Controller/GameCenterController.php
@@ -30,6 +30,11 @@ class GameCenterController extends Controller
                     'url' => 'centrum.csgo',
                     'img' => 'csgo'
                 ],
+                [
+                    'title' => 'Heroes of the Storm',
+                    'url' => 'centrum.hots',
+                    'img' => 'hots'
+                ],
             ]
         ]);
     }
@@ -131,8 +136,30 @@ class GameCenterController extends Controller
      */
     public function hotsAction()
     {
+        $seo = $this->container->get('sonata.seo.page');
+        $seo->setTitle('Heroes of the Storm - Centrum Gracza :: JestemGraczem.pl')
+            ->addMeta('name', 'description', "Centrum każdego gracza Heroes of the Storm! Najświeższe i najciekawsze newsy tylko u nas!")
+            ->addMeta('property', 'og:title', 'Heroes of the Storm - Centrum Gracza :: JestemGraczem.pl')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('centrum.hots', [], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        $feeds = [
+            [
+                'url' => 'http://eu.battle.net/heroes/pl/feed/news',
+                'name' => 'Battle.net'
+            ],
+            [
+                'url' => 'https://hotscenter.pl/rss',
+                'name' => 'HotSCenter.pl'
+            ],
+            [
+                'url' => 'http://mmo24.pl/tag/hots-polska/feed/',
+                'name' => 'MMO24.pl'
+            ]
+        ];
+
         return $this->render('gameCenter/hots.html.twig', [
-            // ...
+            'feeds' => $feeds,
+            'json' => json_encode($feeds,true)
         ]);
     }
 
