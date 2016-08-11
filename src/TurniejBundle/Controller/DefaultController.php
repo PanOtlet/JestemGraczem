ff580cad9ecf76000f9e476ca477d02ee1fab715
Otlet <otlet@protonmail.com>
2016-08-11 11:12:20 +0200

    Początek turniejufff

--- src/TurniejBundle/Controller/DefaultController.php
+++ src/TurniejBundle/Controller/DefaultController.php
@@ -22,7 +22,44 @@ class DefaultController extends Controller
             ->addMeta('property', 'og:title', 'Turnieje')
             ->addMeta('property', 'og:description', 'Więcej informacji wkrótce!')
             ->addMeta('property', 'og:url', $this->get('router')->generate('tournament', [], UrlGeneratorInterface::ABSOLUTE_URL));
-        return $this->render('tournament/index.html.twig',[
+
+        $em = $this->getDoctrine()->getManager();
+        $turnieje = $em->getRepository('TurniejBundle:Turnieje')->findBy(['promoted' => 1]);
+
+        return $this->render('tournament/index.html.twig', [
+            'turnieje' => $turnieje,
+            'color' => $this->color,
+        ]);
+    }
+
+    /**
+     * @Route("/all/{page}", name="tournament.all")
+     */
+    public function allAction($page = 0)
+    {
+        $seo = $this->container->get('sonata.seo.page');
+        $seo->setTitle('Turnieje :: JestemGraczem.pl')
+            ->addMeta('name', 'description', 'Więcej informacji wkrótce!')
+            ->addMeta('property', 'og:title', 'Turnieje')
+            ->addMeta('property', 'og:description', 'Więcej informacji wkrótce!')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('tournament', [], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        $date = new \DateTime("now");
+
+        $em = $this->getDoctrine()->getRepository('TurniejBundle:Turnieje');
+        $query = $em->createQueryBuilder('p')
+            ->where('p.dataStart < :date')
+            ->andWhere('p.dataStop > :date')
+            ->setParameter('date', $date)
+            ->setFirstResult($page * 10)
+            ->orderBy('p.id', 'DESC')
+            ->getQuery()
+            ->setMaxResults(10);
+
+        $turnieje = $query->getResult();
+
+        return $this->render('tournament/all.html.twig', [
+            'turnieje' => $turnieje,
             'color' => $this->color,
         ]);
     }
---
+++ src/TurniejBundle/Entity/Turnieje.php
@@ -0,0 +1,477 @@
+<?php
+
+namespace TurniejBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * turnieje
+ *
+ * @ORM\Table(name="turnieje")
+ * @ORM\Entity(repositoryClass="TurniejBundle\Repository\TurniejeRepository")
+ */
+class Turnieje
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * Nazwa drużyny
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * Opis turnieju
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * Organizator
+     * @ORM\Column(name="owner", type="integer")
+     */
+    private $owner;
+
+    /**
+     * @var int
+     *
+     * Gra, która będzie rozgrywana
+     * @ORM\Column(name="dyscyplina", type="smallint")
+     */
+    private $dyscyplina;
+
+    /**
+     * @var bool
+     *
+     * Open/Invite
+     * @ORM\Column(name="type", type="boolean")
+     */
+    private $type;
+
+    /**
+     * @var int
+     *
+     * Wpisowe, free, czy dotacje?
+     * @ORM\Column(name="cost", type="smallint")
+     */
+    private $cost;
+
+    /**
+     * @var int
+     *
+     * Ilość drużyn
+     * @ORM\Column(name="countTeam", type="smallint")
+     */
+    private $countTeam;
+
+    /**
+     * @var int
+     *
+     * Pula nagród
+     * @ORM\Column(name="prizePool", type="integer")
+     */
+    private $prizePool;
+
+    /**
+     * @var int
+     *
+     * Koszt wpisowego na drużynę
+     * @ORM\Column(name="costPerTeam", type="integer")
+     */
+    private $costPerTeam;
+
+    /**
+     * @var int
+     *
+     * Koszty organizacyjne
+     * @ORM\Column(name="costOrg", type="smallint")
+     */
+    private $costOrg;
+
+    /**
+     * @var \DateTime
+     *
+     * Data rozpoczęcia turnieju
+     * @ORM\Column(name="dataStart", type="date")
+     */
+    private $dataStart;
+
+    /**
+     * @var \DateTime
+     *
+     * Data zakończenia turnieju
+     * @ORM\Column(name="dataStop", type="date")
+     */
+    private $dataStop;
+
+    /**
+     * @var bool
+     *
+     * Czy turniej jest dla drużyn, czy dla graczy
+     * @ORM\Column(name="playerType", type="boolean")
+     */
+    private $playerType;
+
+    /**
+     * @var bool
+     *
+     * Promowany?
+     * @ORM\Column(name="promoted", type="boolean")
+     */
+    private $promoted;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return turnieje
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return turnieje
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param integer $owner
+     *
+     * @return turnieje
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return int
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Set dyscyplina
+     *
+     * @param integer $dyscyplina
+     *
+     * @return turnieje
+     */
+    public function setDyscyplina($dyscyplina)
+    {
+        $this->dyscyplina = $dyscyplina;
+
+        return $this;
+    }
+
+    /**
+     * Get dyscyplina
+     *
+     * @return int
+     */
+    public function getDyscyplina()
+    {
+        return $this->dyscyplina;
+    }
+
+    /**
+     * Set type
+     *
+     * @param boolean $type
+     *
+     * @return turnieje
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return bool
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set cost
+     *
+     * @param integer $cost
+     *
+     * @return turnieje
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    /**
+     * Get cost
+     *
+     * @return int
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * Set countTeam
+     *
+     * @param integer $countTeam
+     *
+     * @return turnieje
+     */
+    public function setCountTeam($countTeam)
+    {
+        $this->countTeam = $countTeam;
+
+        return $this;
+    }
+
+    /**
+     * Get countTeam
+     *
+     * @return int
+     */
+    public function getCountTeam()
+    {
+        return $this->countTeam;
+    }
+
+    /**
+     * Set wpisowe
+     *
+     * @param integer $wpisowe
+     *
+     * @return turnieje
+     */
+    public function setWpisowe($wpisowe)
+    {
+        $this->wpisowe = $wpisowe;
+
+        return $this;
+    }
+
+    /**
+     * Get wpisowe
+     *
+     * @return int
+     */
+    public function getWpisowe()
+    {
+        return $this->wpisowe;
+    }
+
+    /**
+     * Set costPerTeam
+     *
+     * @param integer $costPerTeam
+     *
+     * @return turnieje
+     */
+    public function setCostPerTeam($costPerTeam)
+    {
+        $this->costPerTeam = $costPerTeam;
+
+        return $this;
+    }
+
+    /**
+     * Get costPerTeam
+     *
+     * @return int
+     */
+    public function getCostPerTeam()
+    {
+        return $this->costPerTeam;
+    }
+
+    /**
+     * Set costOrg
+     *
+     * @param integer $costOrg
+     *
+     * @return turnieje
+     */
+    public function setCostOrg($costOrg)
+    {
+        $this->costOrg = $costOrg;
+
+        return $this;
+    }
+
+    /**
+     * Get costOrg
+     *
+     * @return int
+     */
+    public function getCostOrg()
+    {
+        return $this->costOrg;
+    }
+
+    /**
+     * Set dataStart
+     *
+     * @param \DateTime $dataStart
+     *
+     * @return turnieje
+     */
+    public function setDataStart($dataStart)
+    {
+        $this->dataStart = $dataStart;
+
+        return $this;
+    }
+
+    /**
+     * Get dataStart
+     *
+     * @return \DateTime
+     */
+    public function getDataStart()
+    {
+        return $this->dataStart;
+    }
+
+    /**
+     * Set dataStop
+     *
+     * @param \DateTime $dataStop
+     *
+     * @return turnieje
+     */
+    public function setDataStop($dataStop)
+    {
+        $this->dataStop = $dataStop;
+
+        return $this;
+    }
+
+    /**
+     * Get dataStop
+     *
+     * @return \DateTime
+     */
+    public function getDataStop()
+    {
+        return $this->dataStop;
+    }
+
+    /**
+     * Set playerType
+     *
+     * @param boolean $playerType
+     *
+     * @return turnieje
+     */
+    public function setPlayerType($playerType)
+    {
+        $this->playerType = $playerType;
+
+        return $this;
+    }
+
+    /**
+     * Get playerType
+     *
+     * @return bool
+     */
+    public function getPlayerType()
+    {
+        return $this->playerType;
+    }
+
+    /**
+     * @param boolean $promoted
+     */
+    public function setPromoted($promoted)
+    {
+        $this->promoted = $promoted;
+    }
+
+    /**
+     * Get promoted
+     *
+     * @return bool
+     */
+    public function getPromoted()
+    {
+        return $this->promoted;
+    }
+}
+
