8931c8f0213ae87abf16094e05c41ad1ce5eddf9
Otlet <otlet@protonmail.com>
2016-08-11 11:25:04 +0200

    Fix

--- src/TurniejBundle/Controller/DefaultController.php
+++ src/TurniejBundle/Controller/DefaultController.php
@@ -33,16 +33,16 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/all/{page}", name="tournament.all")
+     * @Route("/aktualne/{page}", name="tournament.aktualne")
      */
-    public function allAction($page = 0)
+    public function aktualneAction($page = 0)
     {
         $seo = $this->container->get('sonata.seo.page');
-        $seo->setTitle('Turnieje :: JestemGraczem.pl')
-            ->addMeta('name', 'description', 'Więcej informacji wkrótce!')
-            ->addMeta('property', 'og:title', 'Turnieje')
-            ->addMeta('property', 'og:description', 'Więcej informacji wkrótce!')
-            ->addMeta('property', 'og:url', $this->get('router')->generate('tournament', [], UrlGeneratorInterface::ABSOLUTE_URL));
+        $seo->setTitle('Turnieje w trakcie rozgrywek :: JestemGraczem.pl')
+            ->addMeta('name', 'description', 'Aktualnie odbywające się turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:title', 'Turnieje w trakcie rozgrywek :: JestemGraczem.pl')
+            ->addMeta('property', 'og:description', 'Aktualnie odbywające się turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('tournament.aktualne', ['page' => $page], UrlGeneratorInterface::ABSOLUTE_URL));
 
         $date = new \DateTime("now");
 
@@ -50,6 +50,69 @@ class DefaultController extends Controller
         $query = $em->createQueryBuilder('p')
             ->where('p.dataStart < :date')
             ->andWhere('p.dataStop > :date')
+            ->setParameter('date', $date)
+            ->setFirstResult($page * 10)
+            ->orderBy('p.id', 'DESC')
+            ->getQuery()
+            ->setMaxResults(10);
+
+        $turnieje = $query->getResult();
+
+        return $this->render('tournament/all.html.twig', [
+            'turnieje' => $turnieje,
+            'color' => $this->color,
+        ]);
+    }
+
+    /**
+     * @Route("/zapowiedziane/{page}", name="tournament.zapowiedziane")
+     */
+    public function zapowiedzianeAction($page = 0)
+    {
+        $seo = $this->container->get('sonata.seo.page');
+        $seo->setTitle('Zapowiedziane turnieje :: JestemGraczem.pl')
+            ->addMeta('name', 'description', 'Przyszłe turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:title', 'Turnieje')
+            ->addMeta('property', 'og:description', 'Przyszłe turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('tournament.zapowiedziane', ['page' => $page], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        $date = new \DateTime("now");
+
+        $em = $this->getDoctrine()->getRepository('TurniejBundle:Turnieje');
+        $query = $em->createQueryBuilder('p')
+            ->where('p.dataStart > :date')
+            ->setParameter('date', $date)
+            ->setFirstResult($page * 10)
+            ->orderBy('p.id', 'DESC')
+            ->getQuery()
+            ->setMaxResults(10);
+
+        $turnieje = $query->getResult();
+
+        return $this->render('tournament/all.html.twig', [
+            'turnieje' => $turnieje,
+            'color' => $this->color,
+        ]);
+    }
+
+    /**
+     * @Route("/zakonczone/{page}", name="tournament.zakonczone")
+     */
+    public function zakonczoneAction($page = 0)
+    {
+        $seo = $this->container->get('sonata.seo.page');
+        $seo->setTitle('Zapowiedziane turnieje :: JestemGraczem.pl')
+            ->addMeta('name', 'description', 'Przyszłe turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:title', 'Turnieje')
+            ->addMeta('property', 'og:description', 'Przyszłe turnieje na platformie JestemGraczem.pl!')
+            ->addMeta('property', 'og:url', $this->get('router')->generate('tournament.zapowiedziane', ['page' => $page], UrlGeneratorInterface::ABSOLUTE_URL));
+
+        $date = new \DateTime("now");
+
+        $em = $this->getDoctrine()->getRepository('TurniejBundle:Turnieje');
+        $query = $em->createQueryBuilder('p')
+            ->where('p.dataStop < :date')
+            ->andWhere('p.dataStart < :date')
             ->setParameter('date', $date)
             ->setFirstResult($page * 10)
             ->orderBy('p.id', 'DESC')
