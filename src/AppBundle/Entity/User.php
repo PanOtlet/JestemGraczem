eee8091b304d03dab27a4418ddf2c140f769d144
Otlet <otlet@protonmail.com>
2016-08-16 10:38:31 +0200

    Update

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -88,7 +88,7 @@ class User extends BaseUser
     protected $localization = null;
 
     /**
-     * @ORM\Column(type="string", nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      *
      * @Assert\Length(
      *     min=3,
