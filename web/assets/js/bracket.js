eb3cd45dd4e6d3405748d3c210deb48eb59e8d14
PudzianKing <jmbatkowski@gmail.com>
2016-09-12 12:13:42 +0200

    bracket

--- web/assets/js/bracket.js
+++ web/assets/js/bracket.js
@@ -71,7 +71,15 @@ function saveFn(data, userData) {
         contentType: 'application/json',
         dataType: 'json',
         type: 'post',
-        data: {data: Data ,id: Id}
+        data: {data: Data ,id: Id},
+    success: function(result) {
+            console.log({data: Data ,id: Id});
+            console.log(JSON.stringify({data: Data ,id: Id}));
+        },
+        error:function(exception){
+            console.log({data: Data ,id: Id});
+            console.log(JSON.stringify({data: Data ,id: Id}));
+        }
     });
 
 }
