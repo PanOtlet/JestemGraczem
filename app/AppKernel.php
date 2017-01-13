0652d08ce0c48d2dc7de22babb0f8560b6342a16
Otlet <otlet@protonmail.com>
2017-01-13 14:00:31 +0100

    ADD: SECURITY BUNDLE

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Nelmio\SecurityBundle\NelmioSecurityBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\SeoBundle\SonataSeoBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
